02fbdf9602c0b737b44db792a446cabe42ff803b
Tiffany Foster <tiffanyfstr@outlook.com>
2017-02-03 08:13:24 -0500

    Create input panels

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Learning Vue.js
 
 Simple Vue app with a textbox and checkbox component.
+
+TODO: Add submit.  Proceed to next app panel.
--- app.js
+++ app.js
@@ -6,24 +6,40 @@ Vue.component('text-input',{
 Vue.component('checkbox-input',{
 	props: ['inputinfo'],
 	template: '#checkbox-input-template'
-});
+})
+
+Vue.component('panel-elem',{
+	props: ['panelinfo'],
+	template: '#panel-elem-template'
+})
 
 var app = new Vue({
 	el: '#main',
 	data: {
 		title: 'Vue.js Playground',
 		logo: 'https://vuejs.org/images/logo.png',
-		textboxes : [
-			{ id: 'firstname', label: 'First Name', help: 'Enter your first name'},
-			{ id: 'middlename', label: 'Middle Name', help: 'Enter your middle name'},
-			{ id: 'lastname', label: 'Last Name', help: 'Enter your last name'},
-		],
-		checkboxes : [
-			{ id: 'late', text: 'stay up late'},
-			{ id: 'vacay', text: 'go on vacation'},
-			{ id: 'movies', text: 'go to the movies'},
-			{ id: 'sushi', text: 'eat sushi'},
-			{ id: 'walk', text: 'take long walks'}
+		panels: [
+			{
+				heading: 'What is your name?',
+				textboxes: [
+					{ id: 'firstname', class: 'col-md-4 col-sm-12 s-textinput', label: 'First Name', help: 'Enter your first name'},
+					{ id: 'middlename', class: 'col-md-4 col-sm-12 s-textinput', label: 'Middle Name', help: 'Enter your middle name'},
+					{ id: 'lastname', class: 'col-md-4 col-sm-12 s-textinput', label: 'Last Name', help: 'Enter your last name'},
+				],
+				show: true
+			},
+			{
+				heading: 'What are your favorite colors?',
+				checkboxes: [
+					{ id: 'red', class: 'col-md-3 col-sm-4 col-xs-6 s-checkboxinput', text: 'Red'},
+					{ id: 'green', class: 'col-md-3 col-sm-4 col-xs-6 s-checkboxinput', text: 'Green'},
+					{ id: 'yellow', class: 'col-md-3 col-sm-4 col-xs-6 s-checkboxinput', text: 'Yellow'},
+					{ id: 'blue', class: 'col-md-3 col-sm-4 col-xs-6 s-checkboxinput', text: 'Blue'},
+					{ id: 'orange', class: 'col-md-3 col-sm-4 col-xs-6 s-checkboxinput', text: 'Orange'},
+					{ id: 'purple', class: 'col-md-3 col-sm-4 col-xs-6 s-checkboxinput', text: 'Purple'}
+				],
+				show: false
+			}
 		]
 	}
 })
